3a0b3c35cbfd528a94b08a214fa85aadce4aad41
dengxuyang <673303066@qq.com>
2021-05-12 09:20:52 +0800

    首页布局

---
+++ front/src/components/content/homecard/HomeCard.vue
@@ -0,0 +1,88 @@
+<template>
+  <div
+    :style="{
+      background:
+        'linear-gradient(to right, ' + colorleft + ', ' + colorright + ')',
+    }"
+    class="card"
+  >
+    <div class="left">
+      <div
+        :style="{ backgroundImage: 'url(' + img + ')' }"
+        class="card_icon"
+      ></div>
+    </div>
+    <div class="right">
+      <div>{{info.number}}</div>
+      <div>{{info.text}}</div>
+    </div>
+    <div
+      class="card_reflection"
+      :style="{
+        background: 'linear-gradient(' + colorreflection + ', #ffffff)',
+      }"
+    ></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["colorleft", "colorright", "colorreflection", "icon","info"],
+  data() {
+    return {
+      img: require("@assets/images/" + this.icon + ".png"),
+    };
+  },
+  mounted() {
+    // this.img = require("@assets/images/" + this.icon + ".png");
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.card {
+  //   background: linear-gradient(to right, #64f6b7, #177d1c);
+  height: 150px;
+  border-radius: 8px;
+  text-align: center;
+  color: #ffffff;
+  position: relative;
+  .card_reflection {
+    // background: linear-gradient(#c7f3d7, #ffffff);
+    height: 30px;
+    width: 100%;
+    border-radius: 8px;
+    position: absolute;
+    bottom: -30px;
+  }
+  .left {
+    float: left;
+    width: 50%;
+    height: 150px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .right {
+    width: 50%;
+    height: 150px;
+    display: flex;
+    flex-flow: column;
+    justify-content: center;
+    padding-right: 10px;
+    font-size: 36px;
+    div:nth-child(2) {
+      font-size: 15px;
+    }
+  }
+  .card_icon {
+    width: 60px;
+    height: 60px;
+    background-color: #ffffff;
+    border-radius: 50%;
+    //background-image: url("~@assets/images/icon_vip.png");
+    background-repeat: no-repeat;
+    background-position: center;
+  }
+}
+</style>
--- front/src/views/home/Home.vue
+++ front/src/views/home/Home.vue
@@ -2,99 +2,125 @@
   <div>
     <div class="overall">
       <div class="ovearall_title">一机游整体情况</div>
-      <div>
-        <el-row :gutter="20">
-          <el-col :span="6" :offset="0">
-            <div class="card">
-              <div>会员数量</div>
-              <div>0</div>
-              <div>
-                <!-- <el-button
-                  type="primary"
-                  size="default"
-                  @click="outputplanmodel"
-                  >导出测试</el-button 
-                >-->
-              </div>
-            </div>
-          </el-col>
-          <el-col :span="6" :offset="0">
-            <div class="card">
-              <div>评论数量</div>
-               <div>0</div>
-            </div>
-          </el-col>
-          <el-col :span="6" :offset="0">
-            <div class="card">
-              <div>咨询数量</div>
-               <div>0</div>
-            </div>
-          </el-col>
-          <el-col :span="6" :offset="0">
-            <div class="card">
-              <div>投诉数量</div>
-               <div>0</div>
-            </div>
-          </el-col>
-        </el-row>
+      <div class="card_container">
+        <div>
+          <HomeCard
+            :colorleft="'#64f6b7'"
+            :colorright="'#177d1c'"
+            :colorreflection="'#c7f3d7'"
+            :icon="'icon_vip'"
+            :info="{number:1200,text:'会员数量'}"
+          ></HomeCard>
+        </div>
+        <div>
+          <HomeCard
+            :colorleft="'#43b6ff'"
+            :colorright="'#0168ff'"
+            :colorreflection="'#c3e6ff'"
+            :icon="'icon_message'"
+            :info="{number:856,text:'评论数量'}"
+          ></HomeCard>
+        </div>
+        <div>
+          <HomeCard
+            :colorleft="'#74f4d7'"
+            :colorright="'#03a185'"
+            :colorreflection="'#c6f7ec'"
+            :icon="'icon_consulting'"
+            :info="{number:259,text:'咨询数量'}"
+          ></HomeCard>
+        </div>
+        <div>
+          <HomeCard
+            :colorleft="'#4edbfc'"
+            :colorright="'#0298cc'"
+            :colorreflection="'#c8f1fb'"
+            :icon="'icon_complaints'"
+            :info="{number:19,text:'投诉数量'}"
+          ></HomeCard>
+        </div>
       </div>
     </div>
     <div class="overall">
       <div class="ovearall_title">资源统计</div>
-      <div>
-        <el-row :gutter="20">
-          <el-col :span="6" :offset="0">
-            <div class="card">
-              <div>景点数量</div>
-               <div>0</div>
-            </div>
-          </el-col>
-          <el-col :span="6" :offset="0">
-            <div class="card">
-              <div>美食数量</div>
-              <div>0</div>
-            </div>
-          </el-col>
-          <el-col :span="6" :offset="0">
-            <div class="card">
-              <div>住宿数量</div>
-              <div>0</div>
-            </div>
-          </el-col>
-          <el-col :span="6" :offset="0">
-            <div class="card">
-              <div>娱乐数量</div>
-               <div>0</div>
-            </div>
-          </el-col>
-        </el-row>
-        <el-row :gutter="20">
-          <el-col :span="6" :offset="0">
-            <div class="card">
-              <div>商店数量</div>
-               <div>0</div>
-              <div></div>
-            </div>
-          </el-col>
-          <el-col :span="6" :offset="0">
-            <div class="card">
-              <div>商品数量</div>
-               <div>0</div>
-            </div>
-          </el-col>
-          <el-col :span="6" :offset="0">
-            <div class="card">
-              <div>线路数量</div>
-               <div>0</div>
-            </div>
-          </el-col>
-          <el-col :span="6" :offset="0">
-            <div class="card">
-              <div>门票数量</div>
-               <div>0</div>
-            </div>
-          </el-col>
-        </el-row>
+       <div class="card_container">
+         <div>
+          <HomeCard
+            :colorleft="'#4edbfc'"
+            :colorright="'#0298cc'"
+            :colorreflection="'#c8f1fb'"
+            :icon="'icon_attractions'"
+            :info="{number:45,text:'景点数量'}"
+          ></HomeCard>
+        </div>
+        <div>
+          <HomeCard
+            :colorleft="'#ae95ff'"
+            :colorright="'#753ccb'"
+            :colorreflection="'#e0d4f9'"
+            :icon="'icon_food'"
+            :info="{number:99,text:'美食数量'}"
+          ></HomeCard>
+        </div>
+        <div>
+          <HomeCard
+            :colorleft="'#ead059'"
+            :colorright="'#fd8127'"
+            :colorreflection="'#fae6be'"
+            :icon="'icon_stay'"
+            :info="{number:67,text:'住宿数量'}"
+          ></HomeCard>
+        </div>
+        <div>
+         <div>
+          <HomeCard
+            :colorleft="'#43b6ff'"
+            :colorright="'#0168ff'"
+            :colorreflection="'#c3e6ff'"
+            :icon="'icon_amusement'"
+            :info="{number:245,text:'娱乐数量'}"
+          ></HomeCard>
+        </div>
+        </div>
+      </div>
+       <div class="card_container">
+        <div>
+        <HomeCard
+            :colorleft="'#ead059'"
+            :colorright="'#fd8127'"
+            :colorreflection="'#fae6be'"
+            :icon="'icon_shop'"
+            :info="{number:36,text:'商店数量'}"
+          ></HomeCard>
+        </div>
+        <div>
+         
+          <HomeCard
+            :colorleft="'#74f4d7'"
+            :colorright="'#03a185'"
+            :colorreflection="'#c6f7ec'"
+            :icon="'icon_goods'"
+            :info="{number:3568,text:'商品数量'}"
+          ></HomeCard>
+        </div>
+        <div>
+          <HomeCard
+            :colorleft="'#43b6ff'"
+            :colorright="'#0168ff'"
+            :colorreflection="'#c3e6ff'"
+            :icon="'icon_line'"
+            :info="{number:185,text:'线路数量'}"
+          ></HomeCard>
+        </div>
+        <div>
+          <HomeCard
+            :colorleft="'#4edbfc'"
+            :colorright="'#0298cc'"
+            :colorreflection="'#c8f1fb'"
+            :icon="'icon_ticket'"
+            :info="{number:6500,text:'门票数量'}"
+          ></HomeCard>
+        </div>
       </div>
     </div>
   </div>
@@ -102,7 +128,11 @@
 
 <script>
 import { outputplanmodel } from "@network/basedata.js";
+import HomeCard from "@components/content/homecard/HomeCard";
 export default {
+  components: {
+    HomeCard,
+  },
   methods: {
     outputplanmodel() {
       let params = {
@@ -110,7 +140,6 @@ export default {
         directory_code: "play",
         pageNo: 1,
         pageNum: 10,
-      
       };
       outputplanmodel(params)
         .then((res) => {
@@ -118,7 +147,7 @@ export default {
           let blob = new Blob([res], { type: "application/vnd.ms-excel" });
           link.style.display = "none";
           link.href = URL.createObjectURL(blob);
-        //  link.download = res.headers["content-disposition"];
+          //  link.download = res.headers["content-disposition"];
           link.download = "测试"; //下载的文件名
           document.body.appendChild(link);
           link.click();
@@ -130,32 +159,36 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .el-row {
   margin-bottom: 20px;
 }
 .overall {
   background-color: #fff;
   border-radius: 5px;
-
+  padding-bottom: 30px;
+  padding-left: 30px;
   margin-top: 20px;
   border: 1px solid #f0eeee;
   box-shadow: 0px 0px 10px #d8d5d5;
-  padding: 10px;
 }
 .ovearall_title {
-  margin-bottom: 10px;
+  font-family: MicrosoftYaHei-Bold;
+  font-size: 16px;
+  font-weight: bold;
+  font-stretch: normal;
+  line-height: 30px;
+  letter-spacing: 1px;
+  color: #595757;
+  padding-top: 30px;
+  padding-bottom: 24px;
 }
-.card {
-  background-color: #a29bfe;
-  height: 200px;
-  border-radius: 5px;
-  text-align: center;
-  padding-top: 50px;
-  color: #ffffff;
-}
-.card>:nth-child(2){
-  font-size: 40px;
-  margin-top: 20px;
+.card_container {
+  padding-bottom: 30px;
+  display: flex;
+  div {
+    margin-right: 25px;
+    width: 17vw;
+  }
 }
 </style>
